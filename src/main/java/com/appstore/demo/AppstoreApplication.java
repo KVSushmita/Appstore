b68499a6b657ae70eb242eb34fc3f918dff59a20
KVSushmita <rks.sushmi@gmail.com>
2018-12-27 11:45:08 +0530

    initial project Commit

---
+++ src/main/java/com/appstore/demo/AppstoreApplication.java
@@ -0,0 +1,15 @@
+package com.appstore.Appstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppstoreApplication.class, args);
+		system.out.println("Hello Appstore, Welcome to the year 2019");
+	}
+
+}
+
---
+++ src/test/java/com/appstore/Appstore/AppstoreApplicationTests.java
@@ -0,0 +1,17 @@
+package com.appstore.Appstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
